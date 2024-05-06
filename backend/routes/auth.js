52800fbc8bbcb724c65d19722d7382ab38acf628
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2024-05-06 20:20:20 +0530

    Connected all backend routes and designed a basic ui

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -15,9 +15,10 @@ router.post('/createuser', [
   body('password', 'Password must have a minimum of 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
   //If there are errors, return bad request and the errors
+  let success=false;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({success,errors: errors.array() });
   }
   //check whether the user with this email already exists
 
@@ -25,7 +26,7 @@ router.post('/createuser', [
 
   let user = await User.findOne({email:req.body.email});
   if(user){
-    return res.status(400).json({error:"Sorry a user with this email already exists"})
+    return res.status(400).json({success,error:"Sorry a user with this email already exists"})
   }
   const salt = await bcrypt.genSalt(10);
   const secPass = await bcrypt.hash(req.body.password,salt);
@@ -43,7 +44,8 @@ router.post('/createuser', [
   const authtoken = jwt.sign(data,JWT_SECRET);
 
   //res.json(user)
-  res.json({authtoken}) 
+  success=true;
+  res.json({success,authtoken}) 
   
 } catch (error) {
     console.error(error.message);
@@ -57,6 +59,7 @@ router.post('/login', [
   body('email', "Enter a valid Email").isEmail(),
   body('password', 'Password cannot be blank').exists()
 ], async (req, res) => {
+  let sucess = false;
   //If there are errors, return bad request and the errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -68,11 +71,14 @@ router.post('/login', [
     let user =await User.findOne({email});
     if(!user)
     {
+      success = false;
       return res.status(400).json({error:"Please try to login with correct credentials"});
     }
     const passwordCompare = await bcrypt.compare(password,user.password);
     if(!passwordCompare){
-      return res.status(400).json({error:"Please try to login with correct credentials"});
+      success=false
+      return res.status(400).json({success,error:"Please try to login with correct credentials"});
+    
     }
     const data = {
       user :{
@@ -80,7 +86,8 @@ router.post('/login', [
       }
     }
     const authtoken = jwt.sign(data,JWT_SECRET);
-    res.json({authtoken})
+    success = true;
+    res.json({success,authtoken})
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server error");
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,78 +2,94 @@ import NoteContext from "./noteContext";
 import {useState} from "react";
 
 const NoteState = (props) => {
+    const host = "http://localhost:5000"
 
-    const notesInitial = 
-    [
-        {
-          "_id": "6634c8359acb636eb66775c1",
-          "user": "663260f54f0d4de2944de55e",
-          "title": "My Title",
-          "description": "Please wake up early",
-          "tag": "personal",
-          "date": "2024-05-03T11:19:17.612Z",
-          "__v": 0
-        },
-        {
-          "_id": "6634d587965cc6949be01d8c",
-          "user": "663260f54f0d4de2944de55e",
-          "title": "New note",
-          "description": "Please access the playlist",
-          "tag": "Youtube",
-          "date": "2024-05-03T12:16:07.201Z",
-          "__v": 0
-        },
-        {
-            "_id": "6634c8359acb636eb667735c1",
-            "user": "663260f54f0d4de2944de55e",
-            "title": "My Title",
-            "description": "Please wake up early",
-            "tag": "personal",
-            "date": "2024-05-03T11:19:17.612Z",
-            "__v": 0
-          },
-          {
-            "_id": "6634d5879615cc6949be01d8c",
-            "user": "663260f54f0d4de2944de55e",
-            "title": "New note",
-            "description": "Please access the playlist",
-            "tag": "Youtube",
-            "date": "2024-05-03T12:16:07.201Z",
-            "__v": 0
-          },
-      ]
+    const notesInitial = []
 
       const [notes,setNotes] = useState(notesInitial)
 
-      //Add a Note
-      const addNote = (title,description,tag) =>{
+      //Get all Notes
+      const getNotes = async () =>{
+
         //TODO :  API CALL
-        console.log("Adding a new note");
-       const note = {
-            "_id": "6634d5872961cc694s9be01d8c",
-            "user": "663260f54f0d4de2944de55e",
-            "title": title,
-            "description": description,
-            "tag": tag,
-            "date": "2024-05-03T12:16:07.201Z",
-            "__v": 0
-          };
-        setNotes(notes.concat(note))
+        const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+            method: "GET", 
+            headers: {
+              "Content-Type": "application/json",
+              "auth-token":localStorage.getItem('token')
+            }, 
+          });
+          const json = await response.json()
+        setNotes(json)
+      }
+
+      //Add a Note
+      const addNote = async (title,description,tag) =>{
+
+        //TODO :  API CALL
+        const response = await fetch(`${host}/api/notes/addnote`, {
+            method: "POST", 
+            headers: {
+              "Content-Type": "application/json",
+              "auth-token":localStorage.getItem('token')
+            },
+            body: JSON.stringify({title,description,tag}), 
+          });
+          
+          const note = await response.json();
+          setNotes(notes.concat(note))
+
+        
       }
 
       //Delete a Note
-      const deleteNote = (id) =>{
-        console.log("Deletin the note with id "+ id);
+      const deleteNote = async (id) =>{
+
+        const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+            method: "DELETE", 
+            headers: {
+              "Content-Type": "application/json",
+              "auth-token":localStorage.getItem('token')
+            },
+            
+          });
+          const json = response.json();
+          console.log(json);
         const newNotes = notes.filter((note)=>{return note._id!==id})
         setNotes(newNotes)
       }
 
       //Edit a Note
-      const editNote = (id,title,description,tag) =>{
+      const editNote = async (id,title,description,tag) =>{
+        //API call
         
+  const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+    method: "PUT", 
+    headers: {
+      "Content-Type": "application/json",
+      "auth-token":localStorage.getItem('token')
+    },
+    body: JSON.stringify({title,description,tag}), 
+  });
+  const json = await response.json();
+  console.log(json);
+
+  let newNotes = JSON.parse(JSON.stringify(notes))
+        //Logic to edit in client
+        for (let index = 0; index < newNotes.length; index++) {
+            const element = newNotes[index];
+            if (element._id===id) {
+                newNotes[index].title=title;
+                newNotes[index].description=description;
+                newNotes[index].tag=tag;
+                break;
+            }
+        }
+        
+        setNotes(newNotes);
       }
         return (
-        <NoteContext.Provider value = {{notes,addNote,deleteNote,editNote}}>
+        <NoteContext.Provider value = {{notes,addNote,deleteNote,editNote,getNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
