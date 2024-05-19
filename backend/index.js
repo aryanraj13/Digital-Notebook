c56092cf75dd285b1151fc18c279f8f9d047de85
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2024-05-20 01:01:47 +0530

    Fixes

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ const express = require('express')
 var cors = require('cors')
 connectToMongo();
 
+
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 
 
 app.use(cors())
